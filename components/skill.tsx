19c7172cab60738cb6b655ac9c40da4536511450
medLmtToInfinity <med.lmt012@gmail.com>
2023-10-27 16:51:29 +0100

    editing skills

--- components/skill.tsx
+++ components/skill.tsx
@@ -5,7 +5,7 @@ const Skill: React.FC<SkillInt> = ({ label, icon }) => {
   return (
     <span className='grayscale transition-all duration-300 hover:grayscale-0 cursor-pointer flex flex-col items-center'>
         {icon}
-      <div className='group-hover/icons:text-teal-300 font-bold text-xl text-center mt-3 uppercase font-mono'>
+      <div className='group-hover/icons:text-teal-300 hidden lg:block md:block font-bold text-xl text-center mt-3 uppercase font-mono'>
         {label}
       </div>
     </span>
